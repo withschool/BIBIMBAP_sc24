3f5dac362fd11df2e0f4dd72455d9fb6a01caaa2
rnjsals0905@gmail.com <rnjsals0905@ajou.ac.kr>
2024-04-08 20:19:46 +0900

    BIBIMBAP's first commit

---
+++ bibimbap/src/main/java/com/bibimbap/BibimbapApplication.java
@@ -0,0 +1,13 @@
+package com.bibimbap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibimbapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibimbapApplication.class, args);
+	}
+
+}
