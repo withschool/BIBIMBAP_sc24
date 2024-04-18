0e35f33d1dc1c4b9fe9d41c8de05ac9f9b3acb44
GWONMIN LEE <rnjsals0905@ajou.ac.kr>
2024-04-18 18:55:16 +0900

    chore: cors 관련 설정
    모든 origin으로 설정 변경

---
+++ withSchool/src/main/java/com/withSchool/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.withSchool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+}
