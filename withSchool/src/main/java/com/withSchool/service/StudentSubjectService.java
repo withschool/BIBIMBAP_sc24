c3100e72c62121f1997f3c3b38b1ced317332e7c
GWONMIN LEE <rnjsals0905@ajou.ac.kr>
2024-04-18 19:44:26 +0900

    feat: 중간 저장
    1.
    권한별 과목 표시 controller
    2.
    과목 기본 정보 + 수강 인원 리턴 controller
    3.
    user entity nullable 제거
    4.
    유저가 수강 중인 목록을 리턴 controller
    
    5. 추가 필요
    수강신청 api

---
+++ withSchool/src/main/java/com/withSchool/service/StudentSubjectService.java
@@ -0,0 +1,45 @@
+package com.withSchool.service;
+
+import com.withSchool.dto.StudentSubjectDTO;
+import com.withSchool.entity.StudentSubject;
+import com.withSchool.entity.User;
+import com.withSchool.repository.StudentSubjectRepository;
+import com.withSchool.repository.SubjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentSubjectService {
+    private final StudentSubjectRepository studentSubjectRepository;
+    private final SubjectRepository subjectRepository;
+
+    @Transactional
+    public List<StudentSubjectDTO> findOnesSugang(User user){
+        List<StudentSubject> studentSubjects = studentSubjectRepository.findByStudent(user);
+        List<StudentSubjectDTO> studentSubjectDTOS = new ArrayList<>();
+
+        for (StudentSubject ss : studentSubjects) {
+            StudentSubjectDTO studentSubjectDTO = StudentSubjectDTO.builder()
+                    .ssid(ss.getSsid())
+                    .student(ss.getUser())
+                    .subject(ss.getSubject())
+                    .midtermScore(ss.getMidtermScore())
+                    .finalScore(ss.getFinalScore())
+                    .regDate(ss.getRegDate())
+                    .build();
+
+            studentSubjectDTOS.add(studentSubjectDTO);
+        }
+
+        return studentSubjectDTOS;
+    }
+
+    public List<User> findSugangStudent(Long subjectId) {
+        return studentSubjectRepository.findUsersBySubject(subjectRepository.findById(subjectId).get());
+    }
+}
