920fd164648ee75178ae28acdcc095aced73b396
mindongmindong <dcmin123@ajou.ac.kr>
2024-04-29 16:00:11 +0900

    [FEAT] school list read

--- withSchool/src/test/java/com/withSchool/service/SchoolInformationServiceTests.java
+++ withSchool/src/test/java/com/withSchool/service/SchoolInformationServiceTests.java
@@ -1,9 +1,13 @@
 package com.withSchool.service;
 
+import com.withSchool.entity.school.SchoolInformation;
 import com.withSchool.service.school.SchoolInformationService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 public class SchoolInformationServiceTests {
@@ -13,5 +17,21 @@ public class SchoolInformationServiceTests {
     @Test
     public void delete(){
         schoolInformationService.delete(19L);
+    }
+    @Test
+    public void add(){
+        IntStream.rangeClosed(1,10).forEach(i->{
+            SchoolInformation schoolInformation = SchoolInformation.builder()
+                    .orgRdnda("창현초등학교(수원)" + i)
+                    .atptOfcdcScCode("123"+i)
+                    .sdSchulCode("123"+i)
+                    .build();
+            schoolInformationService.save(schoolInformation);
+        });
+    }
+
+    @Test
+    public void list(){
+         System.out.println(schoolInformationService.findAll());
     }
 }
