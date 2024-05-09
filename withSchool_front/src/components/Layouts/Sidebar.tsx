f6a1fa4bb2ba7bd302f7c34d0b06597b10ca1293
ChanghyunNam (PC) <tabloni0719@ajou.ac.kr>
2024-05-09 17:05:10 +0900

    fix : components 폴더 최종 수정

---
+++ withSchool_front/src/components/Layouts/Sidebar.tsx
@@ -0,0 +1,223 @@
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useLocation } from 'react-router-dom';
+import { toggleSidebar } from '../../store/themeConfigSlice';
+import AnimateHeight from 'react-animate-height';
+import { IRootState } from '../../store';
+import { useState, useEffect } from 'react';
+import IconCaretsDown from '../Icon/IconCaretsDown';
+import IconCaretDown from '../Icon/IconCaretDown';
+import IconMenuDashboard from '../Icon/Menu/IconMenuDashboard';
+import IconMinus from '../Icon/IconMinus';
+import IconMenuChat from '../Icon/Menu/IconMenuChat';
+import IconMenuMailbox from '../Icon/Menu/IconMenuMailbox';
+import IconMenuTodo from '../Icon/Menu/IconMenuTodo';
+import IconMenuNotes from '../Icon/Menu/IconMenuNotes';
+import IconMenuScrumboard from '../Icon/Menu/IconMenuScrumboard';
+import IconMenuContacts from '../Icon/Menu/IconMenuContacts';
+import IconMenuInvoice from '../Icon/Menu/IconMenuInvoice';
+import IconMenuCalendar from '../Icon/Menu/IconMenuCalendar';
+import IconMenuComponents from '../Icon/Menu/IconMenuComponents';
+import IconMenuElements from '../Icon/Menu/IconMenuElements';
+import IconMenuCharts from '../Icon/Menu/IconMenuCharts';
+import IconMenuWidgets from '../Icon/Menu/IconMenuWidgets';
+import IconMenuFontIcons from '../Icon/Menu/IconMenuFontIcons';
+import IconMenuDragAndDrop from '../Icon/Menu/IconMenuDragAndDrop';
+import IconMenuTables from '../Icon/Menu/IconMenuTables';
+import IconMenuDatatables from '../Icon/Menu/IconMenuDatatables';
+import IconMenuForms from '../Icon/Menu/IconMenuForms';
+import IconMenuUsers from '../Icon/Menu/IconMenuUsers';
+import IconMenuPages from '../Icon/Menu/IconMenuPages';
+import IconMenuAuthentication from '../Icon/Menu/IconMenuAuthentication';
+import IconMenuDocumentation from '../Icon/Menu/IconMenuDocumentation';
+
+const Sidebar = () => {
+    const [currentMenu, setCurrentMenu] = useState<string>('');
+    const [errorSubMenu, setErrorSubMenu] = useState(false);
+    const themeConfig = useSelector((state: IRootState) => state.themeConfig);
+    const semidark = useSelector((state: IRootState) => state.themeConfig.semidark);
+    const location = useLocation();
+    const dispatch = useDispatch();
+    const { t } = useTranslation();
+    const toggleMenu = (value: string) => {
+        setCurrentMenu((oldValue) => {
+            return oldValue === value ? '' : value;
+        });
+    };
+
+    useEffect(() => {
+        const selector = document.querySelector('.sidebar ul a[href="' + window.location.pathname + '"]');
+        if (selector) {
+            selector.classList.add('active');
+            const ul: any = selector.closest('ul.sub-menu');
+            if (ul) {
+                let ele: any = ul.closest('li.menu').querySelectorAll('.nav-link') || [];
+                if (ele.length) {
+                    ele = ele[0];
+                    setTimeout(() => {
+                        ele.click();
+                    });
+                }
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        if (window.innerWidth < 1024 && themeConfig.sidebar) {
+            dispatch(toggleSidebar());
+        }
+    }, [location]);
+
+    return (
+        <div className={semidark ? 'dark' : ''}>
+            <nav
+                className={`sidebar fixed min-h-screen h-full top-0 bottom-0 w-[260px] shadow-[5px_0_25px_0_rgba(94,92,154,0.1)] z-50 transition-all duration-300 ${semidark ? 'text-white-dark' : ''}`}
+            >
+                <div className="bg-white dark:bg-black h-full">
+                    <div className="flex justify-between items-center px-4 py-3">
+                        <NavLink to="/" className="main-logo flex items-center shrink-0">
+                            <img className="w-8 ml-[5px] flex-none" src="/assets/images/logo.svg" alt="logo" />
+                            <span className="text-2xl ltr:ml-1.5 rtl:mr-1.5 font-semibold align-middle lg:inline dark:text-white-light">{t('withSchool')}</span>
+                        </NavLink>
+
+                        <button
+                            type="button"
+                            className="collapse-icon w-8 h-8 rounded-full flex items-center hover:bg-gray-500/10 dark:hover:bg-dark-light/10 dark:text-white-light transition duration-300 rtl:rotate-180"
+                            onClick={() => dispatch(toggleSidebar())}
+                        >
+                            <IconCaretsDown className="m-auto rotate-90" />
+                        </button>
+                    </div>
+                    <PerfectScrollbar className="h-[calc(100vh-80px)] relative">
+                        <ul className="relative font-semibold space-y-0.5 p-4 py-0">
+                            <li className="menu nav-item">
+                                <button type="button" className={`${currentMenu === 'dashboard' ? 'active' : ''} nav-link group w-full`} onClick={() => toggleMenu('dashboard')}>
+                                    <div className="flex items-center">
+                                        <IconMenuDashboard
+                                         className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Super Admin')}</span>
+                                    </div>
+
+                                    <div className={currentMenu !== 'dashboard' ? 'rtl:rotate-90 -rotate-90' : ''}>
+                                        <IconCaretDown />
+                                    </div>
+                                </button>
+
+                                <AnimateHeight duration={300} height={currentMenu === 'dashboard' ? 'auto' : 0}>
+                                    <ul className="sub-menu text-gray-500">
+                                        <li>
+                                            <NavLink to="/school_list">{t('학교 목록')}</NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/analytics">{t('메뉴 2')}</NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/finance">{t('메뉴 3')}</NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/crypto">{t('메뉴 4')}</NavLink>
+                                        </li>
+                                    </ul>
+                                </AnimateHeight>
+                            </li>
+
+                            <h2 className="py-3 px-7 flex items-center uppercase font-extrabold bg-white-light/30 dark:bg-dark dark:bg-opacity-[0.08] -mx-4 mb-1">
+                                <IconMinus className="w-4 h-5 flex-none hidden" />
+                                <span>{t('대 주제 1')}</span>
+                            </h2>
+
+                            <li className="nav-item">
+                                <ul>
+                                    <li className="nav-item">
+                                        <NavLink to="/apps/chat" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuChat className="group-hover:!text-primary shrink-0" />
+                                                <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('메뉴 1')}</span>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/apps/mailbox" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuMailbox className="group-hover:!text-primary shrink-0" />
+                                                <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('메뉴 2')}</span>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/apps/todolist" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuTodo className="group-hover:!text-primary shrink-0" />
+                                                <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('메뉴 3')}</span>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/apps/notes" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuNotes className="group-hover:!text-primary shrink-0" />
+                                                <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('메뉴 4')}</span>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/apps/scrumboard" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuScrumboard className="group-hover:!text-primary shrink-0" />
+                                                <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('메뉴 5')}</span>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/apps/contacts" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuContacts className="group-hover:!text-primary shrink-0" />
+                                                <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('메뉴 6')}</span>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                    <li className="menu nav-item">
+                                        <button type="button" className={`${currentMenu === 'invoice' ? 'active' : ''} nav-link group w-full`} onClick={() => toggleMenu('invoice')}>
+                                            <div className="flex items-center">
+                                                <IconMenuInvoice className="group-hover:!text-primary shrink-0" />
+                                                <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('메뉴 7')}</span>
+                                            </div>
+
+                                            <div className={currentMenu !== 'invoice' ? 'rtl:rotate-90 -rotate-90' : ''}>
+                                                <IconCaretDown />
+                                            </div>
+                                        </button>
+
+                                        <AnimateHeight duration={300} height={currentMenu === 'invoice' ? 'auto' : 0}>
+                                            <ul className="sub-menu text-gray-500">
+                                                <li>
+                                                    <NavLink to="/apps/invoice/list">{t('내용 1')}</NavLink>
+                                                </li>
+                                                <li>
+                                                    <NavLink to="/apps/invoice/preview">{t('내용 2')}</NavLink>
+                                                </li>
+                                                <li>
+                                                    <NavLink to="/apps/invoice/add">{t('내용 3')}</NavLink>
+                                                </li>
+                                                <li>
+                                                    <NavLink to="/apps/invoice/edit">{t('내용 4')}</NavLink>
+                                                </li>
+                                            </ul>
+                                        </AnimateHeight>
+                                    </li>
+
+                        
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </PerfectScrollbar>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Sidebar;
