dc9d571c7ebe2874dbf1e505c0e6c52c92a3901c
GWONMIN LEE <rnjsals0905@ajou.ac.kr>
2024-05-09 15:07:11 +0900

    FIX: 학부모의 자식 수강내역 확인 로직 변경

--- withSchool/src/main/java/com/withSchool/service/subject/SubjectService.java
+++ withSchool/src/main/java/com/withSchool/service/subject/SubjectService.java
@@ -79,10 +79,11 @@ public class SubjectService {
     //  1. 부모-학생 매핑 테이블 참조해서 학생 정보 가져오기
     //  2. 학생 정보를 이용해서 듣는 수업을 호출하는
     //    public List<SubjectInfoDTO> findAllSugangByUser(User user) 메서드 호출하기 끝.
-    // TODO 지금 api는 자녀가 한 명일 때만 가능한 api이기 때문에 자녀가 여러 명일 때는 이 api가 쓰이면 안됨
+    // (보류) 지금 api는 자녀가 한 명일 때만 가능한 api이기 때문에 자녀가 여러 명일 때는 이 api가 쓰이면 안됨
+    // 당장은 안 쓰일 거 같음
     @Transactional
-    public List<SubjectInfoDTO> findChildSubjects(User user) {
-        return findAllSugangByUser(studentParentService.findChildByParentId(user));
+    public List<SubjectInfoDTO> findChildSubjects(User child) {
+        return findAllSugangByUser(child);
     }
 
     public Subject saveSubject(String subjectName, User user) {
