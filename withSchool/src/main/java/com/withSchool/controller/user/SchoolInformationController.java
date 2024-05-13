428e7a6199bac05800aa7002c955afc8174553ac
cksqls97 <cksqls97@ajou.ac.kr>
2024-05-13 13:24:25 +0900

    feat 50.2 학부모의 학교 정보 조회 기능 -> 학부모 뿐만 아니라 모든 유저가 자신이 속한 학교의 정보를 조회할 수 있는 기능 구현
    1. SchoolInformation 조회 기능 추가

---
+++ withSchool/src/main/java/com/withSchool/controller/user/SchoolInformationController.java
@@ -0,0 +1,41 @@
+package com.withSchool.controller.user;
+
+import com.withSchool.dto.school.SchoolInformationDTO;
+import com.withSchool.entity.user.User;
+import com.withSchool.service.school.SchoolInformationService;
+import com.withSchool.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@EnableAutoConfiguration
+@RequestMapping("/schools")
+public class SchoolInformationController {
+
+    private final SchoolInformationService schoolInformationService;
+    private final UserService userService;
+
+    @GetMapping("/info)")
+    public ResponseEntity<Map<String, Object>> getSchoolInformation() {
+        Map<String, Object> response = new HashMap<>();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User admin = userService.findById(authentication.getName());
+        Long id = admin.getSchoolInformation().getSchoolId();
+
+        SchoolInformationDTO schoolInformationDTO = schoolInformationService.findById(id);
+        response.put("school-information", schoolInformationDTO);
+
+        return ResponseEntity.ok().body(response);
+    }
+}
