b15496be61f05fe7fd9b8cf5348bf28d747e189e
GWONMIN LEE <rnjsals0905@ajou.ac.kr>
2024-05-18 14:59:16 +0900

    FEAT: API 추가의 테스트 추가

--- withSchool/src/test/java/com/withSchool/service/UserServiceTest.java
+++ withSchool/src/test/java/com/withSchool/service/UserServiceTest.java
@@ -1,14 +1,25 @@
 package com.withSchool.service;
 
+import com.withSchool.dto.user.ResUserDefaultDTO;
 import com.withSchool.dto.user.UserDeleteRequestDTO;
+import com.withSchool.entity.classes.ClassInformation;
+import com.withSchool.entity.school.SchoolInformation;
+import com.withSchool.entity.subject.Subject;
 import com.withSchool.entity.user.User;
+import com.withSchool.repository.classes.ClassRepository;
+import com.withSchool.repository.school.SchoolInformationRepository;
+import com.withSchool.repository.subject.SubjectRepository;
+import com.withSchool.repository.user.UserRepository;
 import com.withSchool.service.user.UserService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
@@ -18,11 +29,36 @@ import java.util.List;
 @Transactional
 public class UserServiceTest {
 
+    SchoolInformation schoolInformation;
+    Subject subject;
+    ClassInformation classInformation;
+    User student;
+    User teacher;
+
+    @Autowired
+    private SchoolInformationRepository schoolInformationRepository;
+    @Autowired
+    private SubjectRepository subjectRepository;
+    @Autowired
+    private ClassRepository classRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void init() {
+        schoolInformation = schoolInformationRepository.save(SchoolInformation.builder().schulNm("아주고등학교").build());
+        subject = subjectRepository.save(Subject.builder().subjectName("수학").grade("3").semester("1").year("2024").schoolInformation(schoolInformation).build());
+        classInformation = classRepository.save(ClassInformation.builder().grade(1).year(2024).inClass(8).schoolInformation(schoolInformation).build());
+        student = userRepository.save(User.builder().id("student").name("student").accountType(0).schoolInformation(schoolInformation).classInformation(classInformation).build());
+        teacher = userRepository.save(User.builder().id("teacher").name("teacher").accountType(1).schoolInformation(schoolInformation).classInformation(classInformation).build());
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(student, student.getPassword()));
+    }
+
     @Autowired
     private UserService userService;
 
     @Test
-    @Rollback(value = false)
     public void delete() {
         List<Long> userId = Arrays.asList(78L, 79L, 80L, 81L, 82L, 83L, 84L);
         UserDeleteRequestDTO dto = UserDeleteRequestDTO.builder()
@@ -66,7 +102,7 @@ public class UserServiceTest {
 
     @Test
     @DisplayName("회원가입 전 학교 PK, 유저 이름, 생년월일, 유저코드를 사용해서 유저 정보 가져오기")
-    public void testLoadUserUsingUserCode(){
+    public void testLoadUserUsingUserCode() {
         // given
         Long schoolId = 9L;
         String userName = "John Doe";
@@ -84,6 +120,19 @@ public class UserServiceTest {
         Assertions.assertEquals("dd1", user.getId());
 
         Assertions.assertNull(fail);
+
+    }
+
+    @Test
+    @DisplayName("해당 학교의 모든 유저 정보 불러오기")
+    public void testFindAllUserBySchool(){
+        // given
+
+        // when
+        List<ResUserDefaultDTO> users = userService.findAllBySchool_SchoolId();
+
+        // then
+        Assertions.assertEquals(2, users.size());
 
     }
 }
