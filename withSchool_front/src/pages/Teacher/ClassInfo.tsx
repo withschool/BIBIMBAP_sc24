743cf92bf189cd3aff34a934aa35031b29a2e188
Hyunsu Choi <mysj923@naver.com>
2024-05-20 05:54:43 +0900

    fix: api 추가

--- withSchool_front/src/pages/Teacher/ClassInfo.tsx
+++ withSchool_front/src/pages/Teacher/ClassInfo.tsx
@@ -1,7 +1,156 @@
+import { Tab } from '@headlessui/react';
+import { Fragment, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Dialog, Transition } from '@headlessui/react';
+import Tippy from '@tippyjs/react';
+import { mappingStudent, listingStudent, getStudentInfoById } from '../../service/parent';
+import { getSchoolNotice, getSchoolNoticeDetail, getSchoolInfo } from '../../service/school';
+import { getClassInfo } from '../../service/class';
+import { getSubjectList } from '../../service/subject';
+import IconCode from '../../components/Icon/IconCode';
+import IconHome from '../../components/Icon/IconHome';
+import IconUser from '../../components/Icon/IconUser';
+import IconPhone from '../../components/Icon/IconPhone';
+import IconInfoCircle from '../../components/Icon/IconInfoCircle';
+import IconTrashLines from '../../components/Icon/IconTrashLines';
+import IconX from '../../components/Icon/IconX';
+import IconAt from '../../components/Icon/IconAt';
+import IconLock from '../../components/Icon/IconLock';
+import IconDownload from '../../components/Icon/IconDownload';
+import IconGallery from '../../components/Icon/IconGallery';
+import Dropdown from '../../components/Dropdown';
+import IconArrowLeft from '../../components/Icon/IconArrowLeft';
+import IconPrinter from '../../components/Icon/IconPrinter';
+import IconFolder from '../../components/Icon/IconFolder';
+import IconZipFile from '../../components/Icon/IconZipFile';
+import IconTxtFile from '../../components/Icon/IconTxtFile';
+
 const ClassInfo = () => {
-    return(
-        <h1>hello</h1>
-    )
+
+    const [modal21, setModal21] = useState(false);
+    const [userCode, setUserCode] = useState('');
+    const [studentList, setStudentList] = useState([]);
+    const [noticeList, setNoticeList] = useState([]);
+    const [ClassInfo, setClassInfo] = useState([]);
+    const [targetStudentInfo, setTargetStudentInfo] = useState('');
+    const [showModal, setShowModal] = useState(false);
+    const [selectedNotice, setSelectedNotice] = useState('');
+    const userInfo = localStorage.getItem('userinfo');
+
+    useEffect(() => {
+        const fetchSubject = async () => {
+            try { 
+                const data = await getSubjectList();
+                console.log(data);
+            } catch (error) {
+                console.error('Error fetching student list:', error);
+            }
+        };
+        fetchSubject();
+    }, []);
+
+    useEffect(() => {
+        const fetchStudents = async () => {
+            try { 
+                const data = await listingStudent();
+                setStudentList(data);
+            } catch (error) {
+                console.error('Error fetching student list:', error);
+            }
+        };
+        fetchStudents();
+    }, []);
+    
+    useEffect(() => {
+        const fetchNoticeData = async () => {
+            try {
+                const data = await getSchoolNotice(localStorage.getItem('TargetStudent'));
+                setNoticeList(data);
+                console.log(data);    
+            } catch (error) {
+                console.error('Error fetching student data:', error);
+            }
+        };
+        fetchNoticeData();
+    }, []);
+
+    useEffect(() => {
+        const fetchClassData = async () => {
+            try {
+                const data = await getClassInfo(targetStudentInfo);
+                setClassInfo(data);
+                console.log(data);    
+            } catch (error) {
+                console.error('Error fetching class data:', error);
+            }
+        };
+        fetchClassData();
+    }, []);
+
+    const handleChange = (userId : string) => {
+        setTargetStudent(userId);
+        console.log(targetStudent);
+    }
+     
+    return (
+        <div>
+            <ul className="flex space-x-2 y-3 rtl:space-x-reverse">
+                <li>
+                    <Link to="/teacher/home" className="text-primary hover:underline">
+                        학부모
+                    </Link>
+                </li>
+                <li className="before:content-['/'] ltr:before:mr-2 rtl:before:ml-2">
+                    <span>기본 정보</span>
+                </li>
+            </ul>
+            <div className="active pt-5">
+                <div>
+                    <form className="border border-[#ebedf2] dark:border-[#191e3a] rounded-md p-4 mb-5 bg-white dark:bg-black">
+                        <h6 className="text-lg font-bold mb-5">학교 기본정보</h6>
+                        <div className="flex flex-col sm:flex-row">
+                            <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 gap-5">
+                                <div>
+                                    <label htmlFor="name">학교명</label>
+                                    <div className="form-input"></div>
+                                </div>
+                                <div>
+                                    <label htmlFor="sex">소속 교육청</label>
+                                    <div className="form-input"></div>
+                                </div>
+                                <div>
+                                    <label htmlFor="id">주소</label>
+                                    <div className="form-input" ></div>
+                                </div>
+                                <div>
+                                    <label htmlFor="phone">공립/사립</label>
+                                    <div className="form-input" ></div>
+                                </div>
+                                <div>
+                                    <label htmlFor="email">학교 홈페이지</label>
+                                    <div className="form-input" >
+                                    </div>
+                                </div>
+                                <div>
+                                    <label htmlFor="email">공학여부</label>
+                                    <div className="form-input" ></div>
+                                </div>
+                                <div>
+                                    <label htmlFor="email">학교 전화번호</label>
+                                    <div className="form-input" ></div>
+                                </div>
+                                <div>
+                                    <label htmlFor="email">학교 팩스번호</label>
+                                    <div className="form-input" ></div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+
 }
 
 export default ClassInfo;
---
+++ withSchool_front/src/service/class.tsx
@@ -0,0 +1,24 @@
+const url = 'http://223.130.134.181:8080';
+
+export const getClassInfo = async (classId: string | null): Promise<any> => {
+    try {
+        const response = await fetch(`${url}/classes/${classId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            const errorMessage = await response.text();
+            console.error('Failed to fetch school notice:', errorMessage);
+            throw new Error(errorMessage);
+        }
+    } catch (error) {
+        console.error('Error fetching school notice:', error);
+        throw error;
+    }
+}
