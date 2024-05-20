363882ab5eee61b606c3f85b2f86d376159ccc09
GWONMIN LEE <rnjsals0905@ajou.ac.kr>
2024-05-20 18:24:25 +0900

    FEAT: subjectservice findAllSubjectByUserSchool 수정

--- withSchool/src/main/java/com/withSchool/service/subject/SubjectService.java
+++ withSchool/src/main/java/com/withSchool/service/subject/SubjectService.java
@@ -38,6 +38,9 @@ public class SubjectService {
             SubjectInfoDTO subjectInfoDTO = SubjectInfoDTO.builder()
                     .subjectName(s.getSubjectName())
                     .subjectId(s.getSubjectId())
+                    .grade(s.getGrade())
+                    .year(s.getYear())
+                    .semester(s.getSemester())
                     .regDate(s.getRegDate())
                     .build();
 
