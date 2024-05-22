530c2bfaf3a2fadafc8e6a220767750561fc228c
cksqls97 <cksqls97@ajou.ac.kr>
2024-05-22 15:38:53 +0900

    feat 20.2
    create, update, delete 테스트 완료 버전
    커밋 내용은 모두 feat 20.2이다.
    
    URI 수정

--- withSchool/src/main/java/com/withSchool/controller/user/LectureNoteController.java
+++ withSchool/src/main/java/com/withSchool/controller/user/LectureNoteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/lecture-notes")
+@RequestMapping("/subject/lecture-notes")
 @RequiredArgsConstructor
 public class LectureNoteController {
 
