4c0077e962e6784f8a99198bdff432fed37263d8
ChanghyunNam (PC) <tabloni0719@ajou.ac.kr>
2024-05-22 20:54:19 +0900

    fix : gitignore 수정 (빌드 제외)

--- withSchool_front/dist/assets/Index-3a3092e0.js
+++
@@ -1,1 +0,0 @@
-import{u as r,r as t,s as o,a as s,j as i}from"./index-e6579e08.js";const f=()=>{const a=r();return t.useEffect(()=>{a(o("Super-Admin"))}),s(e=>e.themeConfig.theme==="dark"||e.themeConfig.isDarkMode),s(e=>e.themeConfig.rtlClass),t.useState(!0),i("div",{})};export{f as default};
